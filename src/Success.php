af16d2bc1ba34d12d1c6e53e71cfea435f1f9cb7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-19 13:55:57 -0400

    Update Success.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Success.php
+++ src/Success.php
@@ -2,61 +2,26 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ResultType;
+namespace Ghostwriter\Result;
 
-use Ghostwriter\ResultType\Contract\ErrorInterface;
-use Ghostwriter\ResultType\Contract\ResultInterface;
-use Ghostwriter\ResultType\Contract\SuccessInterface;
-use Throwable;
-use UnexpectedValueException;
-use function is_callable;
+use Ghostwriter\Result\Contract\SuccessInterface;
 
 /**
  * Represents the result of successful operation.
  *
- * @template    T
+ * @template TSuccess
+ * @extends AbstractResult<TSuccess>
+ * @implements SuccessInterface<TSuccess>
  *
- * @implements  ResultInterface<T>
+ * @see \Ghostwriter\Result\Tests\Unit\SuccessTest
  */
 final class Success extends AbstractResult implements SuccessInterface
 {
-    /** @var mixed */
-    private $value;
-
-    /** @param mixed $value */
-    public function __construct($value)
-    {
-        $this->value = $this->validate($value);
-    }
-
-    public function get(): mixed
-    {
-        return is_callable($this->value) ? ($this->value)() : $this->value;
-    }
-
     /**
-     * @param mixed $value
-     *
-     * @return mixed
+     * @param TSuccess $value
      */
-    public function validate($value)
+    public function __construct(mixed $value)
     {
-        if ($value instanceof self) {
-            /** @var mixed $value */
-            $value = $value->get();
-        }
-
-        /** @var mixed $value */
-        $value = is_callable($value) ? $value() : $value;
-
-        if ($value instanceof ErrorInterface) {
-            throw new UnexpectedValueException($value::class . ' $value was provided.');
-        }
-
-        if ($value instanceof Throwable) {
-            throw new UnexpectedValueException($value::class . ' $value was provided.');
-        }
-
-        return $value;
+        $this->value = $value;
     }
 }
