8c3677e62e4bed08eb7de04acba8fdcd7d8e0b16
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-19 13:53:25 -0400

    Update ResultInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Contract/ResultInterface.php
+++ src/Contract/ResultInterface.php
@@ -2,52 +2,110 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ResultType\Contract;
+namespace Ghostwriter\Result\Contract;
 
-use RuntimeException;
+use Ghostwriter\Option\Contract\OptionInterface;
+use Throwable;
 
 /**
  * @template TSuccess
- * @template TFailure
  */
 interface ResultInterface
 {
     /**
-     * Get the failure option value.
-     *
-     * @return ErrorInterface<TFailure>
+     * Returns Result if the result is Success, otherwise returns the Error value of self.
      */
-    public function error(): ErrorInterface;
+    public function and(self $result): self;
 
     /**
-     * Flat map over the success value.
+     * Calls $function if the result is Success, otherwise returns the Error value of self.
      *
-     * @param callable(TSuccess): self<TSuccess,TFailure> $callback
-     *
-     * @return self<TSuccess,TFailure>
+     * @param callable(Throwable|TSuccess):ResultInterface $function
      */
-    public function flatMap(callable $callback): self;
+    public function andThen(callable $function): self;
 
-    /** @throws RuntimeException None has no value. */
-    public function get(): mixed;
+    /**
+     * Converts from Result<T, E> to Option<E>.
+     */
+    public function error(): OptionInterface;
 
+    /**
+     * Unwraps a result, yielding the content of a Success.
+     *
+     * @throws Throwable
+     */
+    public function expect(Throwable $throwable): mixed;
+
+    /**
+     * Unwraps a result, yielding the content of an Error.
+     *
+     * @throws Throwable
+     */
+    public function expectError(Throwable $throwable): Throwable;
+
+    /**
+     * Returns true if the result is Error.
+     */
     public function isError(): bool;
 
+    /**
+     * Returns true if the result is Success.
+     */
     public function isSuccess(): bool;
 
     /**
-     * Map over the success or error value.
+     * Maps a Result<T,E> to Result<U,E> by applying a function to a contained Success value, leaving an Error value
+     * untouched.
      *
-     * @param callable(TSuccess|TFailure): self<TSuccess,TFailure> $callback
-     *
-     * @return self<TSuccess,TFailure>
+     * @param callable(TSuccess):TSuccess $function
      */
-    public function map(callable $callback): self;
+    public function map(callable $function): self;
 
     /**
-     * Get the success option value.
+     * Maps a Result<T,E> to Result<T,F> by applying a function to a contained Error value, leaving a Success value
+     * untouched.
      *
-     * @return SuccessInterface<TSuccess>
+     * @param callable(Throwable):Throwable $function
      */
-    public function success(): SuccessInterface;
+    public function mapError(callable $function): self;
+
+    /**
+     * Returns $result if the result is Error, otherwise returns the Success value of self.
+     */
+    public function or(self $result): self;
+
+    /**
+     * Calls $function if the result is Error, otherwise returns the Success value of self.
+     *
+     * @param callable(Throwable):ResultInterface $function
+     */
+    public function orElse(callable $function): self;
+
+    /**
+     * Converts from Result<T, E> to Option<T>.
+     */
+    public function success(): OptionInterface;
+
+    /**
+     * Unwraps a result, yielding the content of a Success.
+     */
+    public function unwrap(): mixed;
+
+    /**
+     * Unwraps a result, yielding the content of an Error.
+     */
+    public function unwrapError(): mixed;
+
+    /**
+     * Unwraps a result, yielding the content of a Success. Else, it returns $fallback.
+     */
+    public function unwrapOr(mixed $fallback): mixed;
+
+    /**
+     * Unwraps a result, yielding the content of a Success. If the value is an Error then it calls $function with its
+     * value.
+     *
+     * @param callable(Throwable):mixed $function
+     */
+    public function unwrapOrElse(callable $function): mixed;
 }
