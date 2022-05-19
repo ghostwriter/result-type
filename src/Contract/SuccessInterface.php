5852f7f47acfb29a84ddc8e53b7a252b2f4fe3cf
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-19 13:53:08 -0400

    Update SuccessInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Contract/SuccessInterface.php
+++ src/Contract/SuccessInterface.php
@@ -2,9 +2,13 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ResultType\Contract;
+namespace Ghostwriter\Result\Contract;
 
+/**
+ * @template TSuccess
+ * @implements ResultInterface<TSuccess>
+ */
 interface SuccessInterface extends ResultInterface
 {
-//    public function __construct(mixed $value);
+    public function __construct(mixed $value);
 }
