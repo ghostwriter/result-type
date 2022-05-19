d7df32656c721cfcb43042acc693bab9d2ab9e46
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-19 13:55:51 -0400

    Update Error.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Error.php
+++ src/Error.php
@@ -2,123 +2,22 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ResultType;
+namespace Ghostwriter\Result;
 
-use Ghostwriter\ResultType\Contract\ErrorInterface;
-use Ghostwriter\ResultType\Contract\ResultInterface;
-use Ghostwriter\ResultType\Contract\SuccessInterface;
+use Ghostwriter\Result\Contract\ErrorInterface;
 use Throwable;
 
 /**
- * @template T
- * @template E
- * @extends AbstractResult<T,E>
+ * Represents the result of errorful operation.
+ *
+ * @extends AbstractResult<Throwable>
+ *
+ * @see \Ghostwriter\Result\Tests\Unit\ErrorTest
  */
 final class Error extends AbstractResult implements ErrorInterface
 {
-    private Throwable $throwable;
-
     public function __construct(Throwable $throwable)
     {
-        $this->throwable = $throwable;
-    }
-
-    /**
-     * Get the error option value.
-     *
-     * @return ErrorInterface<E>
-     */
-    public function error(): ErrorInterface
-    {
-        return $this;
-    }
-
-    public function failure(): ErrorInterface
-    {
-        
-    }
-
-    public function flatMap(callable $callback): ResultInterface
-    {
-        
-    }
-
-//
-//    /**
-//     * Flat map over the success value.
-//     *
-//     * @template S
-//     * @template F
-//     *
-//     * @param callable(T):\GrahamCampbell\ResultType\Result<S,F> $f
-//     *
-//     * @return \GrahamCampbell\ResultType\Result<S,F>
-//     */
-//    public function flatMap(callable $f)
-//    {
-//        // @var \GrahamCampbell\ResultType\Result<S,F>
-//        return self::create($this->value);
-//    }
-//
-//    /**
-//     * Map over the success value.
-//     *
-//     * @template S
-//     *
-//     * @param callable(T):S $f
-//     *
-//     * @return \GrahamCampbell\ResultType\Result<S,E>
-//     */
-//    public function map(callable $f)
-//    {
-//        return self::create($this->value);
-//    }
-//
-//    /**
-//     * Map over the error value.
-//     *
-//     * @template F
-//     *
-//     * @param callable(E):F $f
-//     *
-//     * @return \GrahamCampbell\ResultType\Result<T,F>
-//     */
-//    public function mapError(callable $f)
-//    {
-//        return self::create($f($this->value));
-//    }
-//
-//    /**
-//     * Get the success option value.
-//     *
-//     * @return \PhpOption\Option<T>
-//     */
-//    public function success()
-//    {
-//        return None::create();
-//    }
-//
-//    public static function throw(Throwable $throwable): self
-//    {
-//        return new self($throwable);
-//    }
-    public function get(): void
-    {
-        
-    }
-
-    public function map(callable $callback): ResultInterface
-    {
-        
-    }
-
-    public function mapError(callable $callback): ResultInterface
-    {
-        
-    }
-
-    public function success(): SuccessInterface
-    {
-        
+        $this->value = $throwable;
     }
 }
