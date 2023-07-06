e29bdb5a9449161e5ccaa7d1f60a06065057abd2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-07-05 20:14:17 -0500

    Update ErrorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/ErrorTest.php
+++ tests/Unit/ErrorTest.php
@@ -58,7 +58,9 @@ final class ErrorTest extends TestCase
 
     public function testAndThenThrow(): void
     {
-        $result = $this->error->andThen(static fn (): mixed => throw new RuntimeException());
+        $result = $this->error->andThen(static function(): mixed {
+                throw new RuntimeException(__METHOD__);
+            });
 
         self::assertTrue($result->isError());
         self::assertSame($this->error, $result);
