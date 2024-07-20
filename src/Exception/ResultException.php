b7b8278866bc0477ee85f89348eacbd72445511f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-20 10:56:27 -0500

    Update ResultException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/ResultException.php
+++ src/Exception/ResultException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Result\Exception;
 use Ghostwriter\Result\ExceptionInterface;
 use RuntimeException;
 
-final class ResultException extends RuntimeException implements ExceptionInterface {}
+final class ResultException extends RuntimeException implements ExceptionInterface
+{
+}
